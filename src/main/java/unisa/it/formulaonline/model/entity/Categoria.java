4672881a0de60cac57fa7204d952287081899b35
AlfonsoAnzelmo <a.anzelmo5@studenti.unisa.it>
2024-07-02 18:56:31 +0200

    creazione delle entity commento, categoria

--- src/main/java/unisa/it/formulaonline/model/entity/Categoria.java
+++ src/main/java/unisa/it/formulaonline/model/entity/Categoria.java
@@ -1,4 +1,75 @@
 package unisa.it.formulaonline.model.entity;
 
+import java.util.Objects;
+
 public class Categoria {
+
+    private int idCategoria;
+    private String nome, descrizione;
+    private Categoria categoriaPadre;
+    private Lettore creatore;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Categoria getCategoriaPadre() {
+        return categoriaPadre;
+    }
+
+    public void setCategoriaPadre(Categoria categoriaPadre) {
+        this.categoriaPadre = categoriaPadre;
+    }
+
+    public Lettore getCreatore() {
+        return creatore;
+    }
+
+    public void setCreatore(Lettore creatore) {
+        this.creatore = creatore;
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "idCategoria=" + idCategoria +
+                ", nome='" + nome + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", categoriaPadre=" + categoriaPadre +
+                ", creatore=" + creatore +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return idCategoria == categoria.idCategoria && Objects.equals(nome, categoria.nome) && Objects.equals(descrizione, categoria.descrizione) && Objects.equals(categoriaPadre, categoria.categoriaPadre) && Objects.equals(creatore, categoria.creatore);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCategoria, nome, descrizione, categoriaPadre, creatore);
+    }
 }
--- src/main/java/unisa/it/formulaonline/model/entity/Commento.java
+++ src/main/java/unisa/it/formulaonline/model/entity/Commento.java
@@ -1,4 +1,80 @@
 package unisa.it.formulaonline.model.entity;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class Commento {
+
+    private int idCommento;
+    private String corpo;
+
+    private Discussione discussione;
+    private Date dataCommento;
+
+    private Lettore autore;
+
+    public int getIdCommento() {
+        return idCommento;
+    }
+
+    public void setIdCommento(int idCommento) {
+        this.idCommento = idCommento;
+    }
+
+    public String getCorpo() {
+        return corpo;
+    }
+
+    public void setCorpo(String corpo) {
+        this.corpo = corpo;
+    }
+
+    public Discussione getDiscussione() {
+        return discussione;
+    }
+
+    public void setDiscussione(Discussione discussione) {
+        this.discussione = discussione;
+    }
+
+    public Date getDataCommento() {
+        return dataCommento;
+    }
+
+    public void setDataCommento(Date dataCommento) {
+        this.dataCommento = dataCommento;
+    }
+
+    public Lettore getAutore() {
+        return autore;
+    }
+
+    public void setAutore(Lettore autore) {
+        this.autore = autore;
+    }
+
+    @Override
+    public String toString() {
+        return "Commento{" +
+                "idCommento=" + idCommento +
+                ", corpo='" + corpo + '\'' +
+                ", discussione=" + discussione +
+                ", dataCommento=" + dataCommento +
+                ", autore=" + autore +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Commento commento = (Commento) o;
+        return idCommento == commento.idCommento && Objects.equals(corpo, commento.corpo) && Objects.equals(discussione, commento.discussione) && Objects.equals(dataCommento, commento.dataCommento) && Objects.equals(autore, commento.autore);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idCommento, corpo, discussione, dataCommento, autore);
+    }
 }
+
