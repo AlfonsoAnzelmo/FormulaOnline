f0e0567ac78970fb1a0b4c344e2ccc8feedb3ce1
AlfonsoAnzelmo <a.anzelmo5@studenti.unisa.it>
2024-07-02 21:22:18 +0200

    aggiunta CommentoDAO

--- src/main/java/unisa/it/formulaonline/model/dao/CommentoDAO.java
+++ src/main/java/unisa/it/formulaonline/model/dao/CommentoDAO.java
@@ -1,4 +1,101 @@
 package unisa.it.formulaonline.model.dao;
 
+import unisa.it.formulaonline.model.entity.Categoria;
+import unisa.it.formulaonline.model.entity.Commento;
+import unisa.it.formulaonline.model.entity.Discussione;
+import unisa.it.formulaonline.model.entity.Lettore;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommentoDAO {
+    private LettoreDAO lettoreDAO;
+
+    private DiscussioneDAO discussioneDAO;
+
+    public Commento doRetrieveById(int id) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT idCommento, corpo, discussione, dataCommento, autore" +
+                            "  FROM formulaonlinedb.commento c  " +
+                            " WHERE c.idCommento=?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                rs.getInt(1);
+                Commento commento = new Commento();
+                commento.setIdCommento(rs.getInt(1));
+                commento.setCorpo(rs.getString(2));
+
+                Discussione discussione = discussioneDAO.doRetrieveById(rs.getString(3));
+                commento.setDiscussione(discussione);
+
+                commento.setDataCommento(rs.getDate(4));
+
+                Lettore lettore = lettoreDAO.doRetrieveById(rs.getInt(5));
+                commento.setAutore(lettore);
+
+                return commento;
+            }
+            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Commento> doRetrieveAll() {
+        List<Commento> commentoList = new ArrayList<>();
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT c.idCommento, c.corpo,c.discussione, c.dataCommento, c.autore" +
+                            "  FROM formulaonlinedb.commento c");
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Commento commento = new Commento();
+                commento.setIdCommento(rs.getInt(1));
+                commento.setCorpo(rs.getString(2));
+
+                Discussione discussione = discussioneDAO.doRetrieveById(rs.getString(3));
+                commento.setDiscussione(discussione);
+
+                commento.setDataCommento(rs.getDate(4));
+
+                Lettore lettore = lettoreDAO.doRetrieveById(rs.getInt(5));
+                commento.setAutore(lettore);
+
+                commentoList.add(commento);
+
+            }
+            return commentoList;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public void doSave(Commento commento) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO formulaonlinedb.commento (corpo,discussione,dataCommento,autore)" +
+                            "  VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, commento.getCorpo());
+            ps.setInt(2, commento.getDiscussione().getIdDiscussione());
+            ps.setDate(3, (Date) commento.getDataCommento());
+            ps.setInt(4, commento.getAutore().getIdLettore());
+
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("INSERT error.");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
