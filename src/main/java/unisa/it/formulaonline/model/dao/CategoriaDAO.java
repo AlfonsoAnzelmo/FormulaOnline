5a170bf5aaeadf99ee3b1c17af88c895282834bf
AlfonsoAnzelmo <a.anzelmo5@studenti.unisa.it>
2024-07-02 19:03:03 +0200

    creazione classe per la connessione al DB

--- src/main/java/unisa/it/formulaonline/model/dao/CategoriaDAO.java
+++ src/main/java/unisa/it/formulaonline/model/dao/CategoriaDAO.java
@@ -1,4 +1,11 @@
 package unisa.it.formulaonline.model.dao;
 
+import unisa.it.formulaonline.model.entity.Categoria;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoriaDAO {
+
 }
---
+++ src/main/java/unisa/it/formulaonline/model/dao/ConPool.java
@@ -0,0 +1,35 @@
+
+package unisa.it.formulaonline.model.dao;
+
+
+import jakarta.servlet.annotation.WebServlet;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.TimeZone;
+
+@WebServlet(loadOnStartup = 1)
+public class ConPool {
+	private static DataSource datasource;
+
+	public static Connection getConnection() throws SQLException {
+
+		if (datasource == null) {
+			PoolProperties p = new PoolProperties();
+			p.setUrl("jdbc:mysql://localhost:3306/formulaonlinedb?serverTimezone=" + TimeZone.getDefault().getID());
+			p.setDriverClassName("com.mysql.cj.jdbc.Driver");
+			p.setUsername("root");
+			p.setPassword("Galletta$1234");
+			p.setMaxActive(100);
+			p.setInitialSize(10);
+			p.setMinIdle(10);
+			p.setRemoveAbandonedTimeout(60);
+			p.setRemoveAbandoned(true);
+			datasource = new DataSource();
+			datasource.setPoolProperties(p);
+		}
+		return datasource.getConnection();
+	}
+}
