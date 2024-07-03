0ec44d8d33a52728b329dbda7926b3652e2dfd0a
AlfonsoAnzelmo <a.anzelmo5@studenti.unisa.it>
2024-07-03 12:03:10 +0200

    raffinamento operazione di delete di categoria e modifica delle sottocategorie

--- src/main/java/unisa/it/formulaonline/gestioneDiscussione/service/GestioneDiscussioneSerice.java
+++ src/main/java/unisa/it/formulaonline/gestioneDiscussione/service/GestioneDiscussioneSerice.java
@@ -1,4 +1,20 @@
 package unisa.it.formulaonline.gestioneDiscussione.service;
 
+import unisa.it.formulaonline.model.entity.Categoria;
+import unisa.it.formulaonline.model.entity.Commento;
+import unisa.it.formulaonline.model.entity.Discussione;
+
+import java.util.List;
+
 public interface GestioneDiscussioneSerice {
+    Discussione creaDiscussione(Discussione discussione) ;
+    Discussione modificaDiscussione(Discussione discussione, int idDiscussione) ;
+    Discussione cancellaDiscussione(int idDiscussione) ;
+    List<Discussione> ottieniDiscussioniDaCategoria(Categoria categoria);
+
+    Commento creaCommento(Commento commento) ;
+    Commento modificaCommento(Commento commento) ;
+    void rimuoviCommento(Commento commento) ;
+
+
 }
