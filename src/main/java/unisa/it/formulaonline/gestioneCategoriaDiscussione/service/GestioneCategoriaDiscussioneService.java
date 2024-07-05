b7f521979df7d19a47101b78d0ef4e0a43852d50
AlfonsoAnzelmo <a.anzelmo5@studenti.unisa.it>
2024-07-05 10:25:48 +0200

    implementazione del service categoriaDiscussione

--- src/main/java/unisa/it/formulaonline/gestioneCategoriaDiscussione/service/GestioneCategoriaDiscussioneService.java
+++ src/main/java/unisa/it/formulaonline/gestioneCategoriaDiscussione/service/GestioneCategoriaDiscussioneService.java
@@ -1,4 +1,14 @@
 package unisa.it.formulaonline.gestioneCategoriaDiscussione.service;
 
+import unisa.it.formulaonline.model.entity.Categoria;
+import unisa.it.formulaonline.model.entity.Commento;
+import unisa.it.formulaonline.model.entity.Discussione;
+
+import java.util.List;
+
 public interface GestioneCategoriaDiscussioneService {
+
+    Categoria creaCategoriaDiscussione(Categoria categoria) ;
+    Categoria modificaCategoriaDiscussione(Categoria categoria, int idCategoria) ;
+    void cancellaCategoriaDiscussione(int idCategoria) ;
 }
