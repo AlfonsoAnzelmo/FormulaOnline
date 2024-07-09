9fd99a7a144fd148fb14ec95eb2e68ec8d1a6d37
pdantuono <dantuono.paolo1@gmail.com>
2024-07-09 10:46:17 +0200

    typos

--- src/main/java/unisa/it/formulaonline/gestioneDiscussione/controller/AggiornaDiscussioneServlet.java
+++ src/main/java/unisa/it/formulaonline/gestioneDiscussione/controller/AggiornaDiscussioneServlet.java
@@ -13,7 +13,7 @@ import unisa.it.formulaonline.model.entity.Lettore;
 
 import java.io.IOException;
 
-@WebServlet("aggiornaDiscussione")
+@WebServlet("/aggiornaDiscussione")
 public class AggiornaDiscussioneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/java/unisa/it/formulaonline/gestioneDiscussione/controller/EliminaDiscussioneServlet.java
+++ src/main/java/unisa/it/formulaonline/gestioneDiscussione/controller/EliminaDiscussioneServlet.java
@@ -1,6 +1,7 @@
 package unisa.it.formulaonline.gestioneDiscussione.controller;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,6 +11,7 @@ import unisa.it.formulaonline.model.entity.Lettore;
 
 import java.io.IOException;
 
+@WebServlet("/eliminaDiscussione")
 public class EliminaDiscussioneServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
